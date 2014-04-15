fb8dde64b1339f5bf7116b9865f2476cba38b65b
Michael <michael.genesini@gmail.com>
2014-04-15 15:40:22 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snippr::Application.config.secret_key_base = '5379437dd02f7616489fc1e951758a47ba474c356dc824563f7a53c118378cfee502020db1591289fcd816c03d5566f5f8376c0ddde3843262928491698f3ddd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Snippr::Application.config.session_store :cookie_store, key: '_Snippr_session'
